d4b20c2aa2d7500d36a9a3e4055067c533a03aba
darshanpatel9 <14bit033@nirmauni.ac.in>
2018-01-21 16:02:24 +0530

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { GamesComponent } from './games/games.component';
+import { GamelistService } from './gamelist.service';
+import { HttpClientModule } from '@angular/common/http';
+import { GameSearchComponent } from './game-search/game-search.component';
+import { GameDetailComponent } from './game-detail/game-detail.component';
 
+const routes: Routes = [
+  { path: 'games', component: GamesComponent },
+  { path: '', redirectTo: '/games', pathMatch: 'full' },
+  { path: 'detail/:i', component: GameDetailComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamesComponent,
+    GameSearchComponent,
+    GameDetailComponent
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [GamelistService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/game.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by DARSHAN on 21-01-2018.
+ */
+
+export class Game {
+  title: string;
+  platform: string;
+  score: string;
+  genre: string;
+  editors_choice: string;
+}
